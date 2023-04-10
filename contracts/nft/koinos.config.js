492be5ed4901182206ee6118cb5fa3d6d497850c
Julian Gonzalez <joticajulian@gmail.com>
2023-04-10 05:22:35 +0000

    setup networks in scripts

--- contracts/nft/koinos.config.js
+++ contracts/nft/koinos.config.js
@@ -1,3 +1,6 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+
 module.exports = {
   class: "NftContract",
   proto: ["./proto/nft.proto", "./proto/common.proto"],
@@ -5,4 +8,39 @@ module.exports = {
   sourceDir: "./assembly",
   buildDir: "./build",
   koinosProtoDir: "../node_modules/koinos-precompiler-as/koinos-proto",
+  networks: {
+    harbinger: {
+      rpcNodes: [
+        "https://harbinger-api.koinos.io",
+        "https://testnet.koinosblocks.com",
+      ],
+      accounts: {
+        manaSharer: {
+          privateKey: process.env.HARBINGER_MANA_SHARER_PRIVATE_KEY,
+        },
+        contract: {
+          privateKey: process.env.HARBINGER_NFT_CONTRACT_PRIVATE_KEY,
+          id: process.env.HARBINGER_NFT_CONTRACT_ID,
+        },
+        contractOwner: {
+          privateKey: process.env.HARBINGER_NFT_OWNER_PRIVATE_KEY,
+        },
+      },
+    },
+    mainnet: {
+      rpcNodes: ["https://api.koinos.io", "https://api.koinosblocks.com"],
+      accounts: {
+        manaSharer: {
+          privateKey: process.env.MAINNET_MANA_SHARER_PRIVATE_KEY,
+        },
+        contract: {
+          privateKey: process.env.MAINNET_NFT_CONTRACT_PRIVATE_KEY,
+          id: process.env.MAINNET_NFT_CONTRACT_ID,
+        },
+        contractOwner: {
+          privateKey: process.env.MAINNET_NFT_OWNER_PRIVATE_KEY,
+        },
+      },
+    },
+  },
 };
--- contracts/nft/scripts/deployment.ts
+++ contracts/nft/scripts/deployment.ts
@@ -2,19 +2,20 @@ import fs from "fs";
 import path from "path";
 import { Signer, Contract, Provider } from "koilib";
 import { TransactionJson } from "koilib/lib/interface";
-import * as dotenv from "dotenv";
-import tokenAbi from "../build/token-abi.json";
+import abi from "../build/nftcontract-abi.json";
+import koinosConfig from "../koinos.config.js";
 
-dotenv.config();
-
-const privateKeyManaSupporter = process.env.PRIVATE_KEY_MANA_SUPPORTER ?? "";
-const privateKeyContract = process.env.PRIVATE_KEY_CONTRACT ?? "";
+const [inputNetworkName] = process.argv.slice(2);
 
 async function main() {
-  // const provider = new Provider(["http://api.koinos.io:8080"]);
-  const provider = new Provider(["https://api.koinosblocks.com"]);
-  const accountWithFunds = Signer.fromWif(privateKeyManaSupporter);
-  const contractAccount = Signer.fromWif(privateKeyContract);
+  const networkName = inputNetworkName || "harbinger";
+  const network = koinosConfig.networks[networkName];
+  if (!network) throw new Error(`network ${networkName} not found`);
+  const provider = new Provider(network.rpcNodes);
+  const accountWithFunds = Signer.fromWif(
+    network.accounts.manaSharer.privateKey
+  );
+  const contractAccount = Signer.fromWif(network.accounts.contract.privateKey);
   accountWithFunds.provider = provider;
   contractAccount.provider = provider;
 
@@ -22,7 +23,7 @@ async function main() {
     signer: contractAccount,
     provider,
     bytecode: fs.readFileSync(
-      path.join(__dirname, "../build/release/contract.wasm")
+      path.join(__dirname, "../build/release/nft.wasm")
     ),
     options: {
       payer: accountWithFunds.address,
@@ -33,13 +34,14 @@ async function main() {
   });
 
   const { receipt, transaction } = await contract.deploy({
-    abi: JSON.stringify(tokenAbi),
+    abi: JSON.stringify(abi),
+    rcLimit: "10000000000",
   });
   console.log("Transaction submitted. Receipt: ");
   console.log(receipt);
   const { blockNumber } = await transaction.wait("byBlock", 60000);
   console.log(
-    `Contract ${contractAccount.address} uploaded in block number ${blockNumber}`
+    `Contract ${contractAccount.address} uploaded in block number ${blockNumber} (${networkName})`
   );
 }
 
