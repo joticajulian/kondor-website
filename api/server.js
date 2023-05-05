16a395dba381cb6c2b65dc0d4f706db0d2aaf9df
Julian Gonzalez <joticajulian@gmail.com>
2023-05-05 21:36:08 +0000

    minor fix api

--- api/server.js
+++ api/server.js
@@ -181,7 +181,7 @@ fastify.get("/kondor-nfts/:id", async (req, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen(3000, "0.0.0.0");
+    await fastify.listen(8080, "0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
