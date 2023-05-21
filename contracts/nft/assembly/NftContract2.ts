796f68d2ec58a0432910e7fe1caa761157a9d0d0
Julian Gonzalez <joticajulian@gmail.com>
2023-05-21 22:54:09 +0000

    patch nft russia at sc level

--- contracts/nft/assembly/NftContract2.ts
+++ contracts/nft/assembly/NftContract2.ts
@@ -1,13 +1,13 @@
-import { System, Storage, Base58 } from "@koinos/sdk-as";
+import { System, Storage, Base58, Protobuf, StringBytes } from "@koinos/sdk-as";
 import { NftWithMessage } from "./NftWithMessage";
 import { common } from "./proto/common";
 
 export class NftContract2 extends NftWithMessage {
-  dataPatched1: Storage.Obj<common.boole>;
+  dataPatches: Storage.Map<Uint8Array, common.boole>;
 
   constructor() {
     super();
-    this.dataPatched1 = new Storage.Obj(
+    this.dataPatches = new Storage.Map(
       this.contractId,
       9999,
       common.boole.decode,
@@ -17,24 +17,39 @@ export class NftContract2 extends NftWithMessage {
   }
 
   /**
-   * patch data 1
+   * patch data
    * @external
    */
-  patch1(): void {
+  patch(args: common.uint32): void {
     System.require(this.only_owner(), "not authorized by the owner");
-    const dataPatched1 = this.dataPatched1.get()!;
-    System.require(dataPatched1.value == false, "patch1 already done");
+    const key = Protobuf.encode<common.uint32>(args, common.uint32.encode)
+    const dataPatch = this.dataPatches.get(key)!;
+    System.require(dataPatch.value == false, "patch already done");
 
-    /* const address_cynU = Base58.decode("1EWf7YrKt8Yz153rxmVkhXcHPDhMBQcynU");
-    const address_ToN = Base58.decode("1K6oESWG87m3cB3M2WVkzxdTr38po8WToN");
-    const b_cynU = this.balances.get(address_cynU)!;
-    const b_ToN = this.balances.get(address_ToN)!;
-    b_cynU.value += 1;
-    b_ToN.value -= 1;
-    this.balances.put(address_cynU, b_cynU);
-    this.balances.put(address_ToN, b_ToN);
+    switch(args.value) {
+      case 1: {
+        // change Rusia to Russia
+        const tokenId1 = StringBytes.stringToBytes("Rusia");
+        const tokenId2 = StringBytes.stringToBytes("Russia");
+        const auct = this.auctions.get(tokenId1)!;
+        this.auctions.put(tokenId2, auct);
+        this.auctions.remove(tokenId1);
+        break;
+      }
+      case 2: {
+        // change Rusia to Russia fix 2
+        const tokenId = StringBytes.stringToBytes("Russia");
+        const auct = this.auctions.get(tokenId)!;
+        auct.bid!.token_id = tokenId;
+        this.auctions.put(tokenId, auct);
+        break;
+      }
+      default: {
+        System.fail(`patch logic #${args.value} not implemented`);
+      }
+    }
 
-    dataPatched1.value = true;
-    this.dataPatched1.put(dataPatched1); */
+    dataPatch.value = true;
+    this.dataPatches.put(key, dataPatch);
   }
 }
