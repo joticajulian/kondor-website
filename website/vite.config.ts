7384d83491e591bc65c2fcfdbec6abaed2cd6d85
Julian Gonzalez <joticajulian@gmail.com>
2023-06-16 18:16:29 +0200

    fix deployment

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 8080,
+  },
 })
