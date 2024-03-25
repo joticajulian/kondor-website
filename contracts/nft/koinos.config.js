a7200c3ff4e5e8f914f144ec325ed5c4d95c8be0
Julian Gonzalez <joticajulian@gmail.com>
2024-03-25 23:53:12 +0100

    contract: update deps for as27

--- contracts/nft/koinos.config.js
+++ contracts/nft/koinos.config.js
@@ -18,7 +18,30 @@ module.exports = {
   ],
   sourceDir: "./assembly",
   buildDir: "./build",
-  koinosProtoDir: "../node_modules/koinos-precompiler-as/koinos-proto",
+  filesImport: [
+    {
+      dependency: "@koinosbox/contracts",
+      path: "../node_modules/@koinosbox/contracts/assembly/nft/Nft.ts",
+    },
+  ],
+  protoImport: [
+    {
+      name: "@koinosbox/contracts",
+      path:
+        process.env.IMPORT_KOINOSBOX_PROTO_FROM_NODE_MODULES === "true"
+          ? "../node_modules/@koinosbox/contracts/koinosbox-proto"
+          : "../koinosbox-proto",
+      exclude: ["nicknames"],
+    },
+    {
+      name: "@koinos/sdk-as",
+      path: "../node_modules/koinos-precompiler-as/koinos-proto/koinos",
+    },
+    {
+      name: "__",
+      path: "../node_modules/koinos-precompiler-as/koinos-proto/google",
+    },
+  ],
   networks: {
     harbinger: {
       rpcNodes: [
